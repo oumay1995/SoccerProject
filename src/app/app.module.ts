4d47de9fc0c3aae27f30223e75ffdb0e6bd092dc
oumay1995 <rezguioumayma95@gmail.com>
2022-09-21 22:09:34 +0100

    Décomposition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsComponent } from './components/news/news.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { ScoreComponent } from './components/score/score.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewsComponent,
+    VideosComponent,
+    BlogsComponent,
+    MatchesComponent,
+    ScoreComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
